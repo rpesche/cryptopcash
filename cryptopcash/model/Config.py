1fa03bef0686b7e743bd7310dd3c42c1034fe567
Romain PESCHE <romain.pesche@sewan.fr>
2018-03-26 22:44:39 +0200

    get all attribute existing in cryptop

--- cryptopcash/model/Config.py
+++ cryptopcash/model/Config.py
@@ -4,18 +4,38 @@ from cryptopcash.storage.LocalStorage import LocalStorage
 class Config(object):
 
     def __init__(self):
-        self.currency = ''
+        # Theme
+        self.text = 'yellow'
+        self.banner = 'yellow'
+        self.banner_text = 'black'
+        self.background = -1
+        self.dec_places = 2
+        self.field_length = 12
+
+        # API
+        self.currency = 'USD'
+        self.cache = 10
+
+        # Locale
+        self.monetary = None
 
     def load(self):
         storage = LocalStorage()
         parser = storage.get_config()
 
-        self.load_api_section(parser)
+        self.load_section(parser, 'theme', ['text', 'banner', 'banner_text',
+                                            'background', 'dec_places',
+                                            'field_length'])
+        self.load_section(parser, 'api', ['currency', 'cache'])
+        self.load_section(parser, 'locale', ['monetary'])
 
-    def load_api_section(self, parser):
+    def load_section(self, parser, section_name, attributes):
         try:
-            api_config = parser['api']
+            api_config = parser[section_name]
         except KeyError:
             return
 
-        self.currency = api_config.get('currency', self.currency)
+        for attribute in attributes:
+            attribute_value = api_config.get(attribute, None)
+            if attribute_value:
+                self.__setattr__(attribute, attribute_value)
