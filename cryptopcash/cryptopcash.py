70eccbb5361ee2205ef07228750e3b8d5c55e3fe
Romain PESCHE <romain.pesche@sewan.fr>
2018-04-02 12:34:54 +0200

    add syngleton class CryptopCash

--- cryptopcash/cryptopcash.py
+++ cryptopcash/cryptopcash.py
@@ -9,30 +9,50 @@ from cryptopcash.ui.Main import Main
 from cryptopcash.storage.LocalStorage import LocalStorage
 
 
+class CryptopCash(object):
+
+    instance = None
+
+    def __init__(self):
+        self.wallet = Wallet()
+
+        # get Holdings from data file
+        local_storage = LocalStorage()
+        holdings = local_storage.get_holdings()
+        for holding in holdings:
+            self.wallet.add_holding(holding)
+
+        c = CryptoCompare()
+        # Update coin information
+        for holding in holdings:
+            symbol = holding.coin.symbol
+            holding.coin = c.get_coin_info(symbol)
+
+        # load config file
+        self.config = Config()
+        self.config.load()
+
+        # Get price
+        self.market = Market()
+        coins = [holding.coin for holding in holdings]
+        for price in c.get_coins_prices(coins, self.config.currency):
+            self.market.add_currency(price)
+
+    def start_cryptopcash(self):
+        main_ui = Main(self.wallet, self.market, self.config)
+        main_ui.run()
+
+    @staticmethod
+    def get_instance():
+        if not CryptopCash.instance:
+            CryptopCash.instance = CryptopCash()
+        return CryptopCash.instance
+
+
 def main():
-    wallet = Wallet()
-    market = Market()
+    cryptopcash = CryptopCash.get_instance()
+    cryptopcash.start_cryptopcash()
 
-    # get Holdings from data file
-    local_storage = LocalStorage()
-    holdings = local_storage.get_holdings()
-    for holding in holdings:
-        wallet.add_holding(holding)
 
-    c = CryptoCompare()
-    # Update coin information
-    for holding in holdings:
-        symbol = holding.coin.symbol
-        holding.coin = c.get_coin_info(symbol)
-
-    # load config file
-    config = Config()
-    config.load()
-
-    # Get price
-    coins = [holding.coin for holding in holdings]
-    for price in c.get_coins_prices(coins, config.currency):
-        market.add_currency(price)
-
-    main_ui = Main(wallet, market, config)
-    main_ui.run()
+if __name__ == '__main__':
+    main()
