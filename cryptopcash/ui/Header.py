3652c1f0247e2cd3bf758e946140dc345208c773
Romain PESCHE <romain.pesche@sewan.fr>
2018-05-01 14:07:45 +0200

    add alignment

--- cryptopcash/ui/Header.py
+++ cryptopcash/ui/Header.py
@@ -1,12 +1,17 @@
 import urwid
 
 
-HEADERS = ["COIN", "PRICE", "HELD", "VAL", "HIGH", "LOW"]
+HEADERS = [("COIN", 'left'),
+           ("PRICE", 'right'),
+           ("HELD", 'right'),
+           ("VAL", 'right'),
+           ("HIGH", 'right'),
+           ("LOW", 'right')]
 
 
 class Header(urwid.AttrMap):
 
     def __init__(self, palette='highlighted'):
-        columns = [urwid.Text(txt) for txt in HEADERS]
+        columns = [urwid.Text(txt, alignment) for txt, alignment in HEADERS]
         row = urwid.Columns(columns)
         super().__init__(row, palette)
